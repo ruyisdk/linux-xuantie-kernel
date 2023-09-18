14016d4a756c4f791eb8a6fcb7c1b79b080c1e02
Kwanghoon Son <k.son@samsung.com>
2023-09-18 04:51:23 +0000

    dt-bindings: reset: Document th1520 reset control
    Add documentation to describe th1520 reset device
    
    Signed-off-by: Kwanghoon Son <k.son@samsung.com>

---
+++ include/dt-bindings/reset/thead,th1520-reset.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+
+#ifndef DT_BINDING_RESET_TH1520_H
+#define DT_BINDING_RESET_TH1520_H
+
+#define TH1520_RESET_WDT0 0
+#define TH1520_RESET_WDT1 1
+
+#endif
