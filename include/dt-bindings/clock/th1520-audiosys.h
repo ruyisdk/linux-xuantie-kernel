05beb1c490bfbf695c2bdbfc07e35fe383a4b59f
tingming <minghq@linux.alibaba.com>
2024-05-22 16:27:27 +0800

    drivers: clk: to add thead th1520 clk driver

---
+++ include/dt-bindings/clock/th1520-audiosys.h
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2022 Alibaba Group Holding Limited.
+ */
+
+#ifndef _TH1520_AUDIOSYS_H_
+#define _TH1520_AUDIOSYS_H_
+
+#define TH1520_CLKGEN_AUDIO_CPU			      0
+#define TH1520_CLKGEN_AUDIO_SRAM0			      1
+#define TH1520_CLKGEN_AUDIO_SRAM1		      2
+#define TH1520_CLKGEN_AUDIO_DMA                 3
+#define TH1520_CLKGEN_AUDIO_BSM                 4
+#define TH1520_CLKGEN_AUDIO_TIMER                        5
+#define TH1520_CLKGEN_AUDIO_TIMER_CNT1                      6
+#define TH1520_CLKGEN_AUDIO_TIMER_CNT2              7
+#define TH1520_CLKGEN_AUDIO_TIMER_CNT3                       8
+#define TH1520_CLKGEN_AUDIO_TIMER_CNT4                 9
+#define TH1520_CLKGEN_AUDIO_WDR                      10
+#define TH1520_CLKGEN_AUDIO_I2C0                       11
+#define TH1520_CLKGEN_AUDIO_I2C1                      12
+#define TH1520_CLKGEN_AUDIO_UART                   13
+#define TH1520_CLKGEN_AUDIO_I2S0                  14
+#define TH1520_CLKGEN_AUDIO_I2S1                   15
+#define TH1520_CLKGEN_AUDIO_I2S2                  16
+#define TH1520_CLKGEN_AUDIO_I2S8CH                   17
+#define TH1520_CLKGEN_AUDIO_TDM                  18
+#define TH1520_CLKGEN_AUDIO_GPIO                       19
+#define TH1520_CLKGEN_AUDIO_SPDIF0                       20
+#define TH1520_CLKGEN_AUDIO_SPDIF1                21
+#define TH1520_CLKGEN_AUDIO_VAD              22
+#define TH1520_CLKGEN_AUDIO_IOMUX                 23
+#define TH1520_CLKGEN_AUDIO_CLK_END		      24
+
+#endif
