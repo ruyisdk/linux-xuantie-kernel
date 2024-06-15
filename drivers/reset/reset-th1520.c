e2ac1f5d1103f8f91f0c325b74006715be93f30d
David Li <davidli.li@linux.alibaba.com>
2024-06-15 17:16:55 +0800

    i2s: add i2s driver for XuanTie TH1520 SoC
    1. add 'xuantie,th1520-i2s' dt-bindings
    2. add I2S driver code
    3. add I2S reset feature
    4. add I2S nodes in dts
    5. enable I2S config
    
    Signed-off-by: David Li <davidli.li@linux.alibaba.com>

--- drivers/reset/reset-th1520.c
+++ drivers/reset/reset-th1520.c
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0-only
+/*
+ * XuanTie TH1520 System Reset Controller driver
+ *
+ * Copyright (C) 2021 Alibaba Group Holding Limited.
+ *
+ * Author: zenglinghui.zlh <zenglinghui.zlh@linux.alibaba.com>
+ *
+ */
 
 #include <linux/mfd/syscon.h>
 #include <linux/of_device.h>
@@ -25,11 +33,16 @@ struct th1520_rst {
 enum th1520_rst_registers {
 	RST_WDT0 = 0x0034,
 	RST_WDT1 = 0x0038,
+	RST_I2S = 0x01ac,
 	RST_NPU = 0x01b0,
 };
 
 enum th1520_vpsys_rst_registers {
 	RST_FCE = 0x0004,
+};
+
+enum th1520_audiosys_src_registers {
+	RST_AUD_I2S	= 0x0014,
 };
 
 static int th1520_reset_update(struct th1520_rst *rst, unsigned long id,
@@ -44,11 +57,22 @@ static int th1520_reset_update(struct th1520_rst *rst, unsigned long id,
 static const struct th1520_rst_signal th1520_rst_signals[] = {
 	[TH1520_RESET_WDT0] = { RST_WDT0, BIT(0) },
 	[TH1520_RESET_WDT1] = { RST_WDT1, BIT(0) },
+	[TH1520_RESET_HDMI_I2S] = { RST_I2S, BIT(0) },
 	[TH1520_RESET_NPU] = { RST_NPU, BIT(0) },
 };
 
 static const struct th1520_rst_signal th1520_vpsys_rst_signals[] = {
 	[TH1520_RESET_FCE] = { RST_FCE, BIT(0)|BIT(1)|BIT(4)|BIT(5) },
+};
+
+static const struct th1520_rst_signal th1520_audiosys_rst_signals[] = {
+	[TH1520_RESET_AUD_I2S0] = { RST_AUD_I2S, BIT(17) },
+	[TH1520_RESET_AUD_I2S1] = { RST_AUD_I2S, BIT(18) },
+	[TH1520_RESET_AUD_I2S2] = { RST_AUD_I2S, BIT(19) },
+	[TH1520_RESET_AUD_I2S8CH] = { RST_AUD_I2S, BIT(20) },
+	[TH1520_RESET_AUD_TDM] = { RST_AUD_I2S, BIT(21) },
+	[TH1520_RESET_AUD_SPDIF0] = { RST_AUD_I2S, BIT(23) },
+	[TH1520_RESET_AUD_SPDIF1] = { RST_AUD_I2S, BIT(24) },
 };
 
 static struct th1520_rst *to_th1520_rst(struct reset_controller_dev *rcdev)
@@ -93,6 +117,11 @@ static const struct th1520_rst_variant variant_th1520_vpsys = {
 	.signals_num = ARRAY_SIZE(th1520_vpsys_rst_signals),
 };
 
+static const struct th1520_rst_variant variant_th1520_audiosys = {
+	.signals = th1520_audiosys_rst_signals,
+	.signals_num = ARRAY_SIZE(th1520_audiosys_rst_signals),
+};
+
 static int th1520_reset_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
@@ -121,8 +150,9 @@ static int th1520_reset_probe(struct platform_device *pdev)
 }
 
 static const struct of_device_id th1520_reset_dt_ids[] = {
-	{ .compatible = "thead,th1520-reset", .data = &variant_th1520 },
-	{ .compatible = "thead,th1520-vpsys-reset", .data = &variant_th1520_vpsys },
+	{ .compatible = "xuantie,th1520-reset", .data = &variant_th1520 },
+	{ .compatible = "xuantie,th1520-vpsys-reset", .data = &variant_th1520_vpsys },
+	{ .compatible = "xuantie,th1520-audiosys-reset", .data = &variant_th1520_audiosys },
 	{ /* sentinel */ },
 };
 
@@ -136,5 +166,5 @@ static struct platform_driver th1520_reset_driver = {
 builtin_platform_driver(th1520_reset_driver);
 
 MODULE_AUTHOR("zenglinghui.zlh <zenglinghui.zlh@linux.alibaba.com>");
-MODULE_DESCRIPTION("Thead th1520 reset driver");
+MODULE_DESCRIPTION("XuanTie th1520 reset driver");
 MODULE_LICENSE("GPL v2");
