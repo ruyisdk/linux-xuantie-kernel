dbe7c8b232a3d4fc1a5726bae72169404873302a
黄子懿 <huangziyi@canaan-creative.com>
2024-06-25 11:43:20 +0800

    drivers: gpu: add VGLite gpu driver
    Signed-off-by: 黄子懿 <huangziyi@canaan-creative.com>

---
+++ drivers/gpu/vglite/vg_lite_type.h
@@ -0,0 +1,49 @@
+/* SPDX-License-Identifier: (GPL-2.0 OR MIT) */
+/*
+ * Copyright (C) 2024, Canaan Bright Sight Co., Ltd
+ * Copyright (c) 2014 - 2020 Vivante Corporation
+ * All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sub license, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL
+ * THE COPYRIGHT HOLDERS, AUTHORS AND/OR ITS SUPPLIERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * The above copyright notice and this permission notice (including the
+ * next paragraph) shall be included in all copies or substantial portions
+ * of the Software.
+ *
+ */
+
+#ifndef _vg_lite_type_h_
+#define _vg_lite_type_h_
+
+#include <asm/bitsperlong.h>
+#include "vg_lite_kernel.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define VG_FALSE 0
+#define VG_TRUE 1
+
+#define VG_SUCCESS 0
+#define VG_FAIL -1
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
