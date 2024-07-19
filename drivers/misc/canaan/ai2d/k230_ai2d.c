c909e1b9d1d3efa8405cae6b20c279dd15dbcf9b
zhaozhongxiang <zhaozhongxiang@canaan-creative.com>
2024-07-19 19:55:40 +0800

    K230: Add gnne ai2d mmz driver, cma configure

---
+++ drivers/misc/canaan/ai2d/k230_ai2d.c
@@ -0,0 +1,253 @@
+#include <linux/cdev.h>
+#include <linux/io.h>
+#include <linux/kernel.h>
+#include <linux/kfifo.h>
+#include <linux/mm.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/of_irq.h>
+#include <linux/of_platform.h>
+#include <linux/platform_device.h>
+#include <linux/err.h>
+#include <linux/fcntl.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/irqflags.h>
+#include <linux/module.h>
+#include <linux/cpu.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
+#include <linux/cacheinfo.h>
+#include <linux/sizes.h>
+#include <asm/csr.h>
+#include <linux/slab.h>
+#include <linux/device.h>
+#include <linux/irq.h>
+#include <linux/interrupt.h>
+#include <linux/poll.h>
+
+struct ai2d_plat {
+    struct resource *res;
+    void __iomem    *regs;
+    char      *ai2d_reg;
+    int             major;
+    int             minor;
+    int             irq;
+    struct class    *class;
+    struct device   *device;
+    struct cdev     cdev;
+};
+
+static int debug = 1;
+static struct ai2d_plat *plat;
+static uint64_t ai2d_membase[4];
+static unsigned int ai2d_int_flag;
+static int ai2d_fasync_flag = 0;
+DECLARE_WAIT_QUEUE_HEAD(ai2d_waitq);
+struct fasync_struct *ai2d_fasync;
+
+static DEFINE_MUTEX(ai2d_mutex);
+
+static void ai2d_interrupt_clear(void)
+{
+    iowrite32(0x1, (plat->ai2d_reg + 0xa0));
+    iowrite32(0x0, (plat->ai2d_reg + 0xa4));
+    iowrite32(0x0, (plat->ai2d_reg + 0xa8));
+    iowrite32(0x0, (plat->ai2d_reg + 0xac));
+}
+
+static irqreturn_t ai2d_irq(int irq, void *dev_id)
+{
+    ai2d_interrupt_clear();
+    ai2d_int_flag = 1;
+    wake_up_interruptible(&ai2d_waitq);
+
+    if (ai2d_fasync_flag)
+        kill_fasync(&ai2d_fasync, SIGIO, POLL_IN);
+    return IRQ_HANDLED;
+}
+
+static int ai2d_drv_fasync(int fd, struct file *file, int on)
+{
+    int err;
+    err = fasync_helper(fd, file, on, &ai2d_fasync);
+    if(err < 0) {
+        return err;
+    }
+    ai2d_fasync_flag = on;
+
+    return 0;
+}
+
+static unsigned int ai2d_poll(struct file *file, poll_table *wait)
+{
+    unsigned int ret = 0;
+    poll_wait(file, &ai2d_waitq, wait);
+    if(ai2d_int_flag) {
+        ret |= POLLIN;
+        ai2d_int_flag = 0;
+    }
+
+    return ret;
+}
+
+static int ai2d_open(struct inode *inode, struct file *filp)
+{
+    ai2d_int_flag = 0;
+    return 0;
+}
+
+static int ai2d_release(struct inode *inode, struct file *filp)
+{
+    ai2d_int_flag = 0;
+    return 0;
+}
+
+const struct file_operations ai2d_fops = {
+    .owner          = THIS_MODULE,
+    .open           = ai2d_open,
+    .release        = ai2d_release,
+    .poll           = ai2d_poll,
+    .fasync         = ai2d_drv_fasync,
+};
+
+static int ai2d_probe(struct platform_device *pdev)
+{
+    struct resource *res;
+    int err = 0;
+    dev_t dev = 0;
+    int devno;
+    res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+    if (!res) {
+        dev_dbg(&pdev->dev, "ai2d_probe: get resource failed \n");
+        err = -ENODEV;
+        goto error;
+    }
+
+    plat = kzalloc(sizeof(struct ai2d_plat), GFP_KERNEL);
+    if (!plat) {
+        dev_dbg(&pdev->dev, "ai2d_probe: kzalloc failed \n");
+        err = -ENOMEM;
+        goto error;
+    }
+
+    plat->res = res;
+
+    plat->irq = platform_get_irq(pdev, 0);
+    dev_dbg(&pdev->dev, "Gnne irq number is %d\n", plat->irq);
+    if(plat->irq < 0) {
+        dev_dbg(&pdev->dev, "Gnne get irq err\n");
+        err = -ENODEV;
+        goto cleanup_kmem;
+    }
+
+    if((err = request_irq(plat->irq, ai2d_irq, 0, "t0", NULL))) {
+        dev_dbg(&pdev->dev, "Gnne request irq err %d\n", err);
+        goto cleanup_kmem;
+    }
+
+    plat->regs = ioremap(res->start, resource_size(res));
+    if(!plat->regs) {
+        dev_dbg(&pdev->dev, "could not remap register memory\n");
+        err = -ENOMEM;
+        goto cleanup_irq;
+    }
+    plat->ai2d_reg = plat->regs;
+
+    plat->major = 0;
+    plat->minor = 0;
+
+    err = alloc_chrdev_region(&dev, 0, 1, "k230-ai2d");
+    if (err) {
+        dev_dbg(&pdev->dev, "k230-ai2d: can't get major %d\n", plat->major);
+        goto cleanup_ioremap;
+    }
+    plat->major = MAJOR(dev);
+
+    plat->class = class_create("k230_ai2d_class");
+    if (IS_ERR(plat->class)) {
+        err = PTR_ERR(plat->class);
+        goto cleanup_ioremap;
+    }
+
+    devno = MKDEV(plat->major, plat->minor);
+
+    cdev_init(&plat->cdev, &ai2d_fops);
+    plat->cdev.owner = THIS_MODULE;
+    err = cdev_add(&plat->cdev, devno, 1);
+    if (err) {
+        dev_dbg(&pdev->dev, "Error %d adding ai2d device number %d \n", err, plat->minor);
+        goto cleanup_class;
+    }
+
+    plat->device = device_create(plat->class, NULL, devno, NULL, "k230-ai2d");
+    if (IS_ERR(plat->device)) {
+        dev_dbg(&pdev->dev, "device not created\n");
+        err = PTR_ERR(plat->device);
+        goto cleanup_cdev;
+    }
+
+	return 0;
+
+cleanup_device:
+    device_destroy(plat->class, dev);
+cleanup_cdev:
+    cdev_del(&plat->cdev);
+cleanup_class:
+    class_destroy(plat->class);
+cleanup_ioremap:
+    iounmap(plat->regs);
+cleanup_irq:
+    free_irq(plat->irq, NULL);
+cleanup_kmem:
+    kfree(plat);
+error:
+	return err;
+}
+
+static int ai2d_remove(struct platform_device *pdev)
+{
+    dev_t dev = MKDEV(plat->major, plat->minor);
+
+    device_destroy(plat->class, dev);
+    cdev_del(&plat->cdev);
+    class_destroy(plat->class);
+    iounmap(plat->regs);
+    free_irq(plat->irq, NULL);
+    kfree(plat);
+
+    return 0;
+}
+
+static const struct of_device_id k230_ai2d_ids[] = {
+	{ .compatible = "k230-ai2d" },
+	{}
+};
+
+static struct platform_driver k230_ai2d_driver = {
+    .probe          = ai2d_probe,
+    .remove         = ai2d_remove,
+    .driver         = {
+        .name           = "k230-ai2d",
+        .of_match_table = of_match_ptr(k230_ai2d_ids),
+    },
+};
+
+int ai2d_module_init(void)
+{
+    int ret;
+    ret = platform_driver_register(&k230_ai2d_driver);
+    return ret;
+}
+
+void ai2d_module_deinit(void)
+{
+    platform_driver_unregister(&k230_ai2d_driver);
+}
+
+module_init(ai2d_module_init);
+module_exit(ai2d_module_deinit);
+module_param(debug, int, S_IRUGO);
+MODULE_LICENSE("GPL v2");
---
+++ drivers/misc/canaan/mmz/mmz.c
@@ -0,0 +1,301 @@
+/*
+ * kernel module helper for testing MMZ
+ *
+ * Licensed under GPLv2 or later.
+ */
+
+#include <linux/module.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/miscdevice.h>
+#include <linux/dma-mapping.h>
+
+#include <linux/mm.h>
+#include <linux/miscdevice.h>
+#include <linux/slab.h>
+#include <linux/vmalloc.h>
+#include <linux/mman.h>
+#include <linux/random.h>
+#include <linux/init.h>
+#include <linux/tty.h>
+#include <linux/capability.h>
+#include <linux/ptrace.h>
+#include <linux/device.h>
+#include <linux/highmem.h>
+#include <linux/backing-dev.h>
+#include <linux/shmem_fs.h>
+#include <linux/splice.h>
+#include <linux/pfn.h>
+#include <linux/export.h>
+#include <linux/io.h>
+#include <linux/uio.h>
+#include <linux/uaccess.h>
+#include <linux/security.h>
+
+static struct device *mmz_dev;
+static DEFINE_MUTEX(mmz_mutex);
+
+#define MMZ_ALLOC_MEM                         _IOWR('g', 1, unsigned long)
+#define MMZ_FREE_MEM                          _IOWR('g', 2, unsigned long)
+typedef struct {
+    void *user_virt_addr;
+    void *kernel_virt_addr;
+    unsigned long mmz_phys;
+    unsigned long length;
+} mmz_info_t;
+
+static long mmz_ioctl(struct file *filp, unsigned int cmd,
+                             unsigned long arg)
+{
+    mmz_info_t t_mem_info;
+    switch (cmd) {
+        case MMZ_ALLOC_MEM :
+            if(copy_from_user(&t_mem_info, (void *)arg, sizeof(mmz_info_t)))
+                return -EFAULT;
+            t_mem_info.kernel_virt_addr = dma_alloc_coherent(mmz_dev, t_mem_info.length, (dma_addr_t*)(&(t_mem_info.mmz_phys)), GFP_KERNEL | GFP_DMA);
+            if(copy_to_user((void *)arg, &t_mem_info, sizeof(mmz_info_t)))
+                return -EFAULT;
+            break;
+        case MMZ_FREE_MEM :
+            if(copy_from_user(&t_mem_info, (void *)arg, sizeof(mmz_info_t)))
+                return -EFAULT;
+            dma_free_coherent(mmz_dev, t_mem_info.length, t_mem_info.kernel_virt_addr, t_mem_info.mmz_phys);
+            break;
+        default:
+            printk(KERN_ERR "mmz_ioctl: Unknown ioctl: 0x%.8X\n", cmd);
+            return -EINVAL;
+    }
+    return 0;
+}
+
+static long mmz_unlocked_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
+{
+    long ret = 0;
+	mutex_lock(&mmz_mutex);
+    ret = mmz_ioctl(file, cmd, arg);
+	mutex_unlock(&mmz_mutex);
+    return ret;
+}
+
+#ifndef ARCH_HAS_VALID_PHYS_ADDR_RANGE
+static inline int valid_phys_addr_range(phys_addr_t addr, size_t count)
+{
+	return addr + count <= __pa(high_memory);
+}
+
+static inline int valid_mmap_phys_addr_range(unsigned long pfn, size_t size)
+{
+	return 1;
+}
+#endif
+
+#ifndef CONFIG_MMU
+static unsigned long get_unmapped_area_mem(struct file *file,
+					   unsigned long addr,
+					   unsigned long len,
+					   unsigned long pgoff,
+					   unsigned long flags)
+{
+	if (!valid_mmap_phys_addr_range(pgoff, len))
+		return (unsigned long) -EINVAL;
+	return pgoff << PAGE_SHIFT;
+}
+
+/* permit direct mmap, for read, write or exec */
+static unsigned memory_mmap_capabilities(struct file *file)
+{
+	return NOMMU_MAP_DIRECT |
+		NOMMU_MAP_READ | NOMMU_MAP_WRITE | NOMMU_MAP_EXEC;
+}
+
+static unsigned zero_mmap_capabilities(struct file *file)
+{
+	return NOMMU_MAP_COPY;
+}
+
+/* can't do an in-place private mapping if there's no MMU */
+static inline int private_mapping_ok(struct vm_area_struct *vma)
+{
+	return is_nommu_shared_mapping(vma->vm_flags);
+}
+#else
+
+static inline int private_mapping_ok(struct vm_area_struct *vma)
+{
+	return 1;
+}
+#endif
+
+#ifdef CONFIG_STRICT_DEVMEM
+static inline int page_is_allowed(unsigned long pfn)
+{
+	return devmem_is_allowed(pfn);
+}
+static inline int range_is_allowed(unsigned long pfn, unsigned long size)
+{
+	u64 from = ((u64)pfn) << PAGE_SHIFT;
+	u64 to = from + size;
+	u64 cursor = from;
+
+	while (cursor < to) {
+		if (!devmem_is_allowed(pfn))
+			return 0;
+		cursor += PAGE_SIZE;
+		pfn++;
+	}
+	return 1;
+}
+#else
+static inline int page_is_allowed(unsigned long pfn)
+{
+	return 1;
+}
+static inline int range_is_allowed(unsigned long pfn, unsigned long size)
+{
+	return 1;
+}
+#endif
+
+int __weak phys_mem_access_prot_allowed(struct file *file,
+	unsigned long pfn, unsigned long size, pgprot_t *vma_prot)
+{
+	return 1;
+}
+
+#ifdef pgprot_noncached
+static int uncached_access(struct file *file, phys_addr_t addr)
+{
+#if defined(CONFIG_IA64)
+	/*
+	 * On ia64, we ignore O_DSYNC because we cannot tolerate memory
+	 * attribute aliases.
+	 */
+	return !(efi_mem_attributes(addr) & EFI_MEMORY_WB);
+#else
+	/*
+	 * Accessing memory above the top the kernel knows about or through a
+	 * file pointer
+	 * that was marked O_DSYNC will be done non-cached.
+	 */
+	if (file->f_flags & O_DSYNC)
+		return 1;
+	return addr >= __pa(high_memory);
+#endif
+}
+#endif
+
+static pgprot_t phys_mem_access_prot(struct file *file, unsigned long pfn,
+				     unsigned long size, pgprot_t vma_prot)
+{
+#ifdef pgprot_noncached
+	phys_addr_t offset = pfn << PAGE_SHIFT;
+
+	if (uncached_access(file, offset))
+		return pgprot_noncached(vma_prot);
+#endif
+	return vma_prot;
+}
+
+static const struct vm_operations_struct mmap_mem_ops = {
+#ifdef CONFIG_HAVE_IOREMAP_PROT
+	.access = generic_access_phys
+#endif
+};
+
+static int mmz_map(struct file *file, struct vm_area_struct *vma)
+{
+	size_t size = vma->vm_end - vma->vm_start;
+	phys_addr_t offset = (phys_addr_t)vma->vm_pgoff << PAGE_SHIFT;
+
+	/* Does it even fit in phys_addr_t? */
+	if (offset >> PAGE_SHIFT != vma->vm_pgoff)
+		return -EINVAL;
+
+	/* It's illegal to wrap around the end of the physical address space. */
+	if (offset + (phys_addr_t)size - 1 < offset)
+		return -EINVAL;
+
+	if (!valid_mmap_phys_addr_range(vma->vm_pgoff, size))
+		return -EINVAL;
+
+	if (!private_mapping_ok(vma))
+		return -ENOSYS;
+
+	if (!range_is_allowed(vma->vm_pgoff, size))
+		return -EPERM;
+
+	if (!phys_mem_access_prot_allowed(file, vma->vm_pgoff, size,
+						&vma->vm_page_prot))
+		return -EINVAL;
+
+	if(offset < 0x80000000)  // memory set no SO, cached
+	{
+		vma->vm_page_prot.pgprot &= ~(1UL << 63);
+	}
+	else
+	{
+		vma->vm_page_prot = phys_mem_access_prot(file, vma->vm_pgoff,
+						size,
+						vma->vm_page_prot);
+	}
+
+	vma->vm_ops = &mmap_mem_ops;
+
+	/* Remap-pfn-range will mark the range VM_IO */
+	if (remap_pfn_range(vma,
+			    vma->vm_start,
+			    vma->vm_pgoff,
+			    size,
+			    vma->vm_page_prot)) {
+		return -EAGAIN;
+	}
+	return 0;
+}
+
+static int mmap_mem(struct file *file, struct vm_area_struct *vma)
+{
+	int ret = 0;
+	mutex_lock(&mmz_mutex);
+	mmz_map(file, vma);
+	mutex_unlock(&mmz_mutex);
+	return ret;
+}
+
+static const struct file_operations mmz_fops = {
+	.owner = THIS_MODULE,
+	.unlocked_ioctl = mmz_unlocked_ioctl,
+	.mmap		= mmap_mem,
+};
+
+static struct miscdevice mmz_misc = {
+	.name = "mmz",
+	.fops = &mmz_fops,
+};
+
+static int __init mmz_init(void)
+{
+	int ret = 0;
+
+	ret = misc_register(&mmz_misc);
+	if (unlikely(ret)) {
+		pr_err("failed to register mmz test misc device!\n");
+		return ret;
+	}
+	mmz_dev = mmz_misc.this_device;
+	mmz_dev->coherent_dma_mask = ~0;
+	_dev_info(mmz_dev, "registered.\n");
+
+	return ret;
+}
+module_init(mmz_init);
+
+static void __exit mmz_exit(void)
+{
+	misc_deregister(&mmz_misc);
+}
+module_exit(mmz_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Barry Song <21cnbao@gmail.com>");
+MODULE_DESCRIPTION("kernel module to help the test of MMZ");
+MODULE_ALIAS("MMZ test");
