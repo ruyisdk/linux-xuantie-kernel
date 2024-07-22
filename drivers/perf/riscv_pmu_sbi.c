bbd11301abdcfbb1b0cbba7e0ea9143fbdfdd2eb
abing <abing@linux.alibaba.com>
2024-07-22 08:03:34 +0000

    riscv: pmu: add arch_id impid if in errata_probe_pmu for k230
    k230 CPU has an old arch_id and impid
    we add other if for k230, so that soc could get pmu irq and perf func
    could work
    
    Signed-off-by: abing <abing@linux.alibaba.com>

--- drivers/perf/riscv_pmu_sbi.c
+++ drivers/perf/riscv_pmu_sbi.c
@@ -811,9 +811,16 @@ static int pmu_sbi_setup_irqs(struct riscv_pmu *pmu, struct platform_device *pde
 		riscv_pmu_irq_num = RV_IRQ_PMU;
 		riscv_pmu_use_irq = true;
 	} else if (IS_ENABLED(CONFIG_ERRATA_THEAD_PMU) &&
-		   riscv_cached_mvendorid(0) == THEAD_VENDOR_ID &&
-		   riscv_cached_marchid(0) == 0 &&
-		   riscv_cached_mimpid(0) == 0) {
+			riscv_cached_mvendorid(0) == THEAD_VENDOR_ID &&
+			riscv_cached_marchid(0) == 0 &&
+			riscv_cached_mimpid(0) == 0) {
+		riscv_pmu_irq_num = THEAD_C9XX_RV_IRQ_PMU;
+		riscv_pmu_use_irq = true;
+	} else if (IS_ENABLED(CONFIG_ERRATA_THEAD_PMU) &&
+			riscv_cached_mvendorid(0) == THEAD_VENDOR_ID &&
+			riscv_cached_marchid(0) == 0x8000000009140d00 &&
+			riscv_cached_mimpid(0) == 0x50000) {
+		/* For Canaan k230 */
 		riscv_pmu_irq_num = THEAD_C9XX_RV_IRQ_PMU;
 		riscv_pmu_use_irq = true;
 	}
