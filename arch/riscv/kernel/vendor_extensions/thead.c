2ebe14b05757075407e038b76d0ea36fc19833d3
Chen Pei <cp0613@linux.alibaba.com>
2026-03-04 20:34:41 +0800

    riscv: isa: Add xuantie ext support
    XuanTie C930 provides several XuanTie extensions, such as xtheadvdot,
    xtheadvarith, etc. This patch implements parsing support for these
    XuanTie ISA extensions, and also implements hwprobe support to
    facilitate user-space programs in acquiring these capabilities.
    
    Signed-off-by: Chen Pei <cp0613@linux.alibaba.com>

--- arch/riscv/kernel/vendor_extensions/thead.c
+++ arch/riscv/kernel/vendor_extensions/thead.c
@@ -11,6 +11,16 @@
 /* All T-Head vendor extensions supported in Linux */
 static const struct riscv_isa_ext_data riscv_isa_vendor_ext_thead[] = {
 	__RISCV_ISA_EXT_DATA(xtheadvector, RISCV_ISA_VENDOR_EXT_XTHEADVECTOR),
+	__RISCV_ISA_EXT_DATA(xtheadvdot, RISCV_ISA_VENDOR_EXT_XTHEADVDOT),
+	__RISCV_ISA_EXT_DATA(xtheadvarith, RISCV_ISA_VENDOR_EXT_XTHEADVARITH),
+	__RISCV_ISA_EXT_DATA(xtheadvcrypto, RISCV_ISA_VENDOR_EXT_XTHEADVCRYPTO),
+	__RISCV_ISA_EXT_DATA(xtheadvcoder, RISCV_ISA_VENDOR_EXT_XTHEADVCODER),
+	__RISCV_ISA_EXT_DATA(xtheadvfofp8min, RISCV_ISA_VENDOR_EXT_XTHEADVFOFP8MIN),
+	__RISCV_ISA_EXT_DATA(xtheadvfoe8m0min, RISCV_ISA_VENDOR_EXT_XTHEADVFOE8M0MIN),
+	__RISCV_ISA_EXT_DATA(xtheadvfofp4min, RISCV_ISA_VENDOR_EXT_XTHEADVFOFP4MIN),
+	__RISCV_ISA_EXT_DATA(xtheadcbop, RISCV_ISA_VENDOR_EXT_XTHEADCBOP),
+	__RISCV_ISA_EXT_DATA(xtheadaioe, RISCV_ISA_VENDOR_EXT_XTHEADAIOE),
+	__RISCV_ISA_EXT_DATA(xtheadcrc, RISCV_ISA_VENDOR_EXT_XTHEADCRC),
 };
 
 struct riscv_isa_vendor_ext_data_list riscv_isa_vendor_ext_list_thead = {
